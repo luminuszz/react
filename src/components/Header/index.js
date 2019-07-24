6c53c06b8b1b198dd0a864aee07cb70f15652c30
davi ribeiro <luminuszz43@gmail.com>
2019-07-24 17:38:53 -0300

    axios

---
+++ src/components/Header/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import './style.css';
+class Header extends Component {
+
+    render() {
+        return <header id="header_main">Davi</header>
+    }
+
+}
+
+export default Header;
